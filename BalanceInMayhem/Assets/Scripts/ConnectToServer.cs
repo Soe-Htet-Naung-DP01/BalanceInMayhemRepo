cf34742b0050817d6ca330fad6a51655f94a05f7
Soe-Htet-Naung <shn.dev.portfolio.001@gmail.com>
2022-02-11 14:19:51 +0800

    established connection to server.

---
+++ BalanceInMayhem/Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    public InputField usernameInput;
+    public Text buttonText;
+    public Text warningText;
+
+    public void OnClickConnect()
+    {
+        if(usernameInput.text.Length >= 1) //Check If username is not blank
+        {
+            PhotonNetwork.NickName = usernameInput.text; //Get Nickname
+            buttonText.text = "Connecting...";
+            PhotonNetwork.ConnectUsingSettings(); //Photon's built in function to connect to photon server.
+        }
+        else //if username is blank
+        {
+            warningText.text = "A username is required to Connect to the Server!";
+        }
+    }
+
+    public override void OnConnectedToMaster() //Upon establishing a successful connection
+    {
+        PhotonNetwork.LoadLevel("Lobby"); //Change to Lobby Scene.
+    }
+
+}
