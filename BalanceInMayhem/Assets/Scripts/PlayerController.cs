3b854e06c964422d596dd434e2e6cc72d8b98290
Soe-Htet-Naung <shn.dev.portfolio.001@gmail.com>
2022-03-01 17:05:59 +0800

    Synced Movements

--- BalanceInMayhem/Assets/Scripts/PlayerController.cs
+++ BalanceInMayhem/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    PhotonView view;
 
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Vector2 moveDistance = movementInput.normalized * speed * Time.deltaTime;
-        transform.position += (Vector3)moveDistance;
+        if(view.IsMine)
+        {
+            Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            Vector2 moveDistance = movementInput.normalized * speed * Time.deltaTime;
+            transform.position += (Vector3)moveDistance;
+        }
+        
     }
 }
