70129c88cacb6adfc000b78c01f5265f1478e023
Soe-Htet-Naung <shn.dev.portfolio.001@gmail.com>
2022-03-02 16:19:29 +0800

    Health Script and Dmg Animation

--- BalanceInMayhem/Assets/Scripts/Health.cs
+++ BalanceInMayhem/Assets/Scripts/Health.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
 public class Health : MonoBehaviour
 {
+    public int healthPoints = 100;
+    public Text hpDisplay;
+    PhotonView view; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        view = GetComponent<PhotonView>();  
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TakeDmg()
+    {
+        view.RPC("TakeDmgRPC", RpcTarget.All);
+    }
+
+    [PunRPC]
+    void TakeDmgRPC()
+    {
+        healthPoints--;
+        hpDisplay.text = healthPoints.ToString();
     }
 }
--- BalanceInMayhem/Assets/Scripts/PlayerController.cs
+++ BalanceInMayhem/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,12 @@ public class PlayerController : MonoBehaviour
     public float speed;
     Animator anim;
     PhotonView view;
-
+    Health healthScript;
     private void Start()
     {
         view = GetComponent<PhotonView>();
         anim = GetComponent<Animator>();
+        healthScript = FindObjectOfType<Health>();
     }
 
     // Update is called once per frame
@@ -36,6 +37,30 @@ public class PlayerController : MonoBehaviour
                 anim.SetBool("isWalking", true);
             }
 
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(view.IsMine)
+        {
+            if (collision.tag == "Enemy")
+            {
+                anim.SetBool("isTakingDmg", true);
+                healthScript.TakeDmg();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(view.IsMine)
+        {
+            if(collision.tag == "Enemy")
+            {
+                anim.SetBool("isTakingDmg", false);
+            }
         }
         
     }
